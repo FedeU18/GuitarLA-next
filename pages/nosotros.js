aeaf94ad0f10538e8741023d5c00cc2ec183ca2e
Federico <fedee.unates.2001@gmail.com>
2023-05-31 15:43:28 -0300

    aprendiendo a agregar fuentes en next

---
+++ pages/nosotros.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Nosotros = () => {
+  return (
+    <div>
+      desde nosotros <Link href={"/"}>inicio</Link>
+    </div>
+  );
+};
+
+export default Nosotros;
